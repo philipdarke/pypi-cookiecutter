36c105f2c18a81fd770f379103c5a1283cd6a32c
Philip Darke <43066442+philipdarke@users.noreply.github.com>
2022-05-15 13:06:41 +0100

    Rename test directory

---
+++ {{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -0,0 +1,10 @@
+import {{ cookiecutter.package_name }}
+
+
+class TestExamples:
+    def test_function(self):
+        assert {{ cookiecutter.package_name }}.a_function(1, 2)
+
+    def test_class(self):
+        a_class = {{ cookiecutter.package_name }}.AClass(1, 2, 3)
+        assert a_class.a_method(3, 4)
