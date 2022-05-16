0b3c442b0dab2a4776791376ff9f64b389b10aa4
Philip Darke <43066442+philipdarke@users.noreply.github.com>
2022-05-16 13:57:21 +0100

    Remove tests

--- {{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.package_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -2,10 +2,6 @@ from {{ cookiecutter.package_name }} import ExampleClass
 
 
 class TestExampleClass:
-    def test_instance(self):
-        test = ExampleClass("1", 2, 3.0)
-        assert test.tokens == ["1", 2, 3.0]
-
     def test_example_method(self):
         test = ExampleClass("4", 5, 6.0)
         assert test.example_method("4")
